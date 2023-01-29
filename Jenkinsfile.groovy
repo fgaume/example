099d7fb481eb4bd61ddfadd3775af60b2db20312
fgaume <fgaume@gmail.com>
2023-01-29 19:10:07 +0100

    trigger

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,33 @@
+// Define your secret project token here
+def project_token = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEF'
+
+// Reference the GitLab connection name from your Jenkins Global configuration (https://JENKINS_URL/configure, GitLab section)
+properties([
+        gitLabConnection('gitlab-9090'),
+        pipelineTriggers([
+                [
+                        $class: 'GitLabPushTrigger',
+                        branchFilterType: 'All',
+                        triggerOnPush: true,
+                        triggerOnMergeRequest: false,
+                        triggerOpenMergeRequestOnPush: "never",
+                        triggerOnNoteRequest: true,
+                        noteRegex: "Jenkins please retry a build",
+                        skipWorkInProgressMergeRequest: true,
+                        secretToken: project_token,
+                        ciSkip: false,
+                        setBuildDescription: true,
+                        addNoteOnMergeRequest: true,
+                        addCiMessage: true,
+                        addVoteOnMergeRequest: true,
+                        acceptMergeRequestOnSuccess: false,
+                        branchFilterType: "NameBasedFilter",
+                        includeBranchesSpec: "",
+                        excludeBranchesSpec: "",
+                ]
+        ])
+])
+
 pipeline {
 
     agent any
